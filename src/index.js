e61db59c611639da1f2fbf8209ddfdaed411d943
Olivier-Kango <olivierkango@gmail.com>
2022-11-08 21:14:23 -0800

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//component file
-import TodoContainer from "./components/TodoContainer"
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+import { BrowserRouter as Router } from "react-router-dom";
+
+//component
+import TodoContainer from "./functionBased/components/TodoContainer"
+
+//stylesheet
+import "./functionBased/App.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>  
+  </React.StrictMode>, 
+  document.getElementById("root")
+);
